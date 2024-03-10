c23a741f57f38a805e26d908fdc4db654c018de4
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-03-10 01:36:17 -0500

    Fixed root node to point forwards under the hips.

--- addons/godot_mvn_tracker/mvn_source.gd
+++ addons/godot_mvn_tracker/mvn_source.gd
@@ -84,8 +84,9 @@ func _on_mvn_packet(data : MvnBody.JointData) -> void:
 			_body_tracker.set_joint_flags(body, 0)
 			continue
 
-		# If pelvis then consider position calibration
+		# Special handling for pelvis
 		if native == MvnBody.Joint.PELVIS:
+			# Apply position-mode controls
 			match _position_mode:
 				PositionMode.CALIBRATE:
 					# Calibrate on first position
@@ -110,10 +111,15 @@ func _on_mvn_packet(data : MvnBody.JointData) -> void:
 		# Set the joint flags
 		_body_tracker.set_joint_flags(body, JOINT_TRACKING)
 
-	# Calculate and set the root joint under the hips
-	var root := _body_tracker.get_joint_transform(XRBodyTracker.JOINT_HIPS)
-	root.basis = Basis.IDENTITY
-	root.origin = root.origin.slide(Vector3.UP)
+	# Get the hips transform
+	var hips := _body_tracker.get_joint_transform(XRBodyTracker.JOINT_HIPS)
+
+	# Construct the root under the hips pointing forwards
+	var root_y = Vector3.UP
+	var root_z = -hips.basis.x.cross(root_y)
+	var root_x = root_y.cross(root_z)
+	var root_o := hips.origin.slide(Vector3.UP)
+	var root := Transform3D(root_x, root_y, root_z, root_o).orthonormalized()
 	_body_tracker.set_joint_transform(XRBodyTracker.JOINT_ROOT, root)
 	_body_tracker.set_joint_flags(XRBodyTracker.JOINT_ROOT, JOINT_TRACKING)
 
